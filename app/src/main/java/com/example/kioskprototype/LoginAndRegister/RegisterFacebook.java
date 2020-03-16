3fb7201d023271ed453c19f43ca797197f4692ca
arnovn <arnovanneste96@gmail.com>
2020-03-16 18:41:20 +0100

    Facebook button added

--- app/src/main/java/com/example/kioskprototype/LoginAndRegister/RegisterFacebook.java
+++ app/src/main/java/com/example/kioskprototype/LoginAndRegister/RegisterFacebook.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.kioskprototype.R;
 
@@ -15,16 +17,24 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
+import com.facebook.FacebookSdk;
+import com.facebook.login.DeviceLoginManager;
+import com.facebook.login.widget.DeviceLoginButton;
+import com.facebook.login.widget.LoginButton;
 
 
 public class RegisterFacebook extends AppCompatActivity {
+
+    DeviceLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_facebook);
 
+        final ViewGroup viewGroup = (ViewGroup) ((ViewGroup) this.findViewById(android.R.id.content)).getChildAt(0);
 
+        loginButton = (DeviceLoginButton)viewGroup.findViewById(R.id.login_button);
     }
 
 
