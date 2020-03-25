901d93e6113da6a9416b6dd785f2ea5a74deeb85
arnovn <arnovanneste96@gmail.com>
2020-03-25 14:29:32 +0100

    Update RequestMemberCard.java
    Documentation & no warnings.

--- app/src/main/java/com/example/kioskprototype/AccountSettings/RequestMemberCard.java
+++ app/src/main/java/com/example/kioskprototype/AccountSettings/RequestMemberCard.java
@@ -1,8 +1,8 @@
 package com.example.kioskprototype.AccountSettings;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,15 +23,52 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URI;
 
+/**
+ * Class in charge of handling member card requests.
+ *  - Address needs to be inputted by the user
+ *  - Confirmation popup
+ *  - Address needs to be updated in the database.
+ */
 public class RequestMemberCard extends AppCompatActivity implements ConfirmAddressDialog.ConfirmAddressDialogListener{
 
+    /**
+     * TAG so the MapBox fragment knows where to be shown.
+     */
     static final String TAG = "searchTag";
+
+    /**
+     * Confirmation dialog for the address inputted.
+     */
     ConfirmAddressDialog confirmAddressDialog;
+
+    /**
+     * Address of the user is stored in this String.
+     */
     String address;
+
+    /**
+     * Id of the user inside the MySql Database.
+     */
     int id;
+
+    /**
+     * Mail of the user stored inside the MySql Database.
+     */
     String mail;
+
+    /**
+     * When something goes wrong during the insertion of the address inside the database we return
+     * to the previous activity (AccountSettings) passing this code.
+     */
     final int RESULT_FAILED = 2;
 
+    /**
+     * When the activity is created:
+     *  - Gather the user's settings.
+     *  - Initialize the autocomplete fragment (where the user will search his address).
+     * @param savedInstanceState
+     *          Bundle containing the activity's previously saved states
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +76,6 @@ public class RequestMemberCard extends AppCompatActivity implements ConfirmAddre
 
         id = getIntent().getIntExtra("Id",0);
         mail = getIntent().getStringExtra("Mail");
-        Toast.makeText(getApplicationContext(), "Id:" + id, Toast.LENGTH_LONG).show();
 
         PlaceAutocompleteFragment autocompleteFragment;
 
@@ -55,6 +91,7 @@ public class RequestMemberCard extends AppCompatActivity implements ConfirmAddre
                     getSupportFragmentManager().findFragmentByTag(TAG);
         }
 
+        assert autocompleteFragment != null;
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(CarmenFeature carmenFeature) {
@@ -73,20 +110,41 @@ public class RequestMemberCard extends AppCompatActivity implements ConfirmAddre
         });
     }
 
+    /**
+     * Implementation for ConfirmAddressDialog confirmPressed.
+     *  - We get the address passed to the dialog
+     *  - We update the user's address in the MySql Database.
+     */
     @Override
     public void confirmPressed() {
         address = confirmAddressDialog.getAddress();
         new ConnectionInsertAddress().execute();
     }
 
+    /**
+     * Implementation for ConfirmAddressDialog cancelPressed.
+     *  - We return to previous activity passing the RESULT_CANCELED code.
+     */
     @Override
     public void cancelPressed() {
         setResult(RESULT_CANCELED);
         finish();
     }
 
+    /**
+     * Class in charge of inserting the address into the MySql Database for the given user id.
+     */
+    @SuppressLint("StaticFieldLeak")
     class ConnectionInsertAddress extends AsyncTask<String, String, String> {
         String result = "";
+
+        /**
+         * Method in charge of querying the database through an HTTP request.
+         * @param strings
+         *          Paramaters passed when the execution of the AsyncTask is called;
+         * @return
+         *          Returns the response of the database.
+         */
         @Override
         protected String doInBackground(String... strings) {
             String convertedAddress = address.replaceAll("\\s", "%20");
@@ -99,22 +157,27 @@ public class RequestMemberCard extends AppCompatActivity implements ConfirmAddre
                 HttpResponse response = client.execute(request);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
-                StringBuffer stringBuffer = new StringBuffer("");
+                StringBuilder stringBuffer = new StringBuilder();
 
-                String line ="";
+                String line;
                 while((line = reader.readLine()) != null){
                     stringBuffer.append(line);
-                    break;
                 }
                 reader.close();
                 result = stringBuffer.toString();
             }catch (Exception e) {
                 System.out.println("The exception: "+e.getMessage());
-                return new String("The exception: " + e.getMessage());
+                return "The exception: " + e.getMessage();
             }
             return result;
         }
 
+        /**
+         * Method in charge of handling the result gathered from the database.
+         * If insertion was succesfull we send a verification mail & return to previous activity passing the RESULT_OK code.
+         * @param s
+         *          Parameters passed when the AsyncTask has finished.
+         */
         @Override
         protected void onPostExecute(String s) {
             System.out.println(result);
@@ -135,21 +198,20 @@ public class RequestMemberCard extends AppCompatActivity implements ConfirmAddre
         }
     }
 
+    /**
+     * Sends the verification mail to mail couple to the user id.
+     */
     public void sendVerificationMail(){
-        Runnable mailRunnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    GmailSender sender = new GmailSender("wowkioskmail@gmail.com",
-                            "kioskmail123");
-                    sender.sendMail("WOW kiosk Verification mail",
-                            "You've succesfully requested a new membercard.\n \n"
-                                    + "This card will be delivered to: " + address +". ",
-                            "wowkioskmail@gmail.com", mail);
-                } catch (Exception e) {
-                    Log.e("SendMail", e.getMessage(), e);
-                    System.out.println("The exception :" + e);
-                }
+        Runnable mailRunnable = () -> {
+            try {
+                GmailSender sender = new GmailSender("wowkioskmail@gmail.com",
+                        "kioskmail123");
+                sender.sendMail("WOW kiosk Verification mail",
+                        "You've succesfully requested a new membercard.\n \n"
+                                + "This card will be delivered to: " + address +". ",
+                        "wowkioskmail@gmail.com", mail);
+            } catch (Exception e) {
+                System.out.println("The exception :" + e);
             }
         };
         Thread mailThread = new Thread(mailRunnable);
